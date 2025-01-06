a51c355644b9db95f0014311ba9165ac36996cb8
Afsar-WSL <23f2002023@ds.study.iitm.ac.in>
2025-01-06 00:14:37 +0000

    commit on 06-01-25

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,9 @@
-import './assets/main.css'
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(store).use(router).mount("#app");
